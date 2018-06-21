93a25b63b3bb63000c9620e790649ca1233972e0
David <ledavid64@gmail.com>
2018-06-21 12:29:05 -0700

    Handling Errors Locally

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,40 +9,50 @@ import "./Blog.css";
 class Blog extends Component {
   state = {
     posts: [],
-    selectedPostId: null
+    selectedPostId: null,
+    error: false
   };
   componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-      const posts = response.data.slice(0, 4);
-      const updatedPosts = posts.map(post => {
-        return {
-          ...post,
-          author: "David"
-        };
-      });
-      this.setState({ posts: updatedPosts });
-    });
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "David"
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch(error =>
+        // console.log(error)
+        this.setState({ error: true })
+      );
   }
 
-  postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id})
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
   };
 
   render() {
-    const posts = this.state.posts.map(post => (
+    let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p> 
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => (
         <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
         />
-    ));
+      ));
+    }
 
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost id={this.state.selectedPostId}/>
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
