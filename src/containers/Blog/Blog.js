7d71fdd18e3b787e4959719e86b79326bdbd8af8
David <ledavid64@gmail.com>
2018-06-26 10:44:09 -0700

    Styling the Active Route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Blog.css';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import Posts from '../Blog/Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
@@ -12,10 +12,20 @@ class Blog extends Component {
           <nav>
             <ul>
               <li>
-                <Link to="/">Home</Link>
+                <NavLink
+                  to="/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: 'pink',
+                    textDecoration: 'underline'
+                  }}
+                >
+                  Home
+                </NavLink>
               </li>
               <li>
-                <Link
+                <NavLink
                   to={{
                     pathname: 'test',
                     hash: '#submit',
@@ -23,10 +33,10 @@ class Blog extends Component {
                   }}
                 >
                   Test
-                </Link>
+                </NavLink>
               </li>
               <li>
-                <Link to="/new-post">New Post</Link>
+                <NavLink to="/new-post">New Post</NavLink>
               </li>
             </ul>
           </nav>
