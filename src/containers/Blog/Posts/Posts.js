9b2f5a27605fa5a0dafaa8154425453c23767d42
David <ledavid64@gmail.com>
2018-06-26 11:01:26 -0700

    Passing Route Parameters

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
-
+import { Link } from 'react-router-dom';
 class Posts extends Component {
   state = {
     posts: []
@@ -36,12 +36,13 @@ class Posts extends Component {
     let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
     if (!this.state.error) {
       posts = this.state.posts.map(post => (
-        <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}
-        />
+        <Link to={'/' + post.id} key={post.id}>
+          <Post
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        </Link>
       ));
     }
 
